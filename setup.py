dc51069c491cfea9abc612a1ff5a7f21c9d25810
Alex Van't Hof <alexvh@cs.columbia.edu>
2013-08-26 21:55:43 -0400

    Bump version to 0.3

--- setup.py
+++ setup.py
@@ -7,12 +7,15 @@ except ImportError:
 
 setup(
     name='gitlab3',
-    version='0.2.1',
+    version='0.3.0',
     license='LGPLv3',
     description='GitLab API v3 Python Wrapper.',
+    long_description='A complete Python client/wrapper for GitLab API v3',
     packages=['gitlab3'],
     author="Alex Van't Hof",
     author_email='alexvh@cs.columbia.edu',
     install_requires=['requests'],
     url='http://github.com/alexvh/python-gitlab3',
+    bugtrack_url='http://github.com/alexvh/python-gitlab3/issues',
+    keywords='gitlab api client wrapper',
 )
