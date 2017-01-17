a440ea81a34afaa7a9282ef2ce62c2a0f6c61f88
Martin Owens <doctormo@gmail.com>
2017-01-16 21:32:33 -0500

    Update setup to point to new git repository and bump version

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ except ImportError:
 
 setup(
     name='gitlab3',
-    version='0.5.4',
+    version='0.5.5',
     license='LGPLv3',
     description='GitLab API v3 Python Wrapper.',
-    long_description='A complete Python client/wrapper for GitLab API v3',
+    long_description='An incomplete Python client/wrapper for GitLab API v3',
     packages=['gitlab3'],
     author="Alex Van't Hof",
     author_email='alexvh@cs.columbia.edu',
     install_requires=['requests'],
-    url='http://github.com/alexvh/python-gitlab3',
+    url='http://github.com/doctormo/python-gitlab3',
     keywords='gitlab api client wrapper',
 )
